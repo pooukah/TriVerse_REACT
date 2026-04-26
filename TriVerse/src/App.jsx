5f0cdf4a6d309f496502e5cb1fffb0af5a007863
Ray <ray.palacios96@gmail.com>
2026-04-27 00:58:06 +0200

    Fent pàgina de creació de reviews Refs #3

--- TriVerse/src/App.jsx
+++ TriVerse/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Perfil from './components/Perfil';
+import ResetPassword from './components/ResetPassword';
+import Reviews from './components/Reviews';
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/perfil" element={<Perfil />} />
+        <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/reviews" element={<Reviews />} />
       </Routes>
     </BrowserRouter>
   )
