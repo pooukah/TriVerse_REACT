2264fcef8edcc23cb32c81e888ed875924328f75
Kiara Reyes <kiarareyesleiva@gmail.com>
2026-04-27 19:11:13 +0200

    mediapage a medias

--- TriVerse/src/components/mediapage/MediaPage.jsx
+++ TriVerse/src/components/mediapage/MediaPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './MediaPage.css';
 import MediaCard from '../components/mediacard/MediaCard';
 
-const MediaPage = () =>{
+const MediaPage = ({title, data}) =>{
     return(
-        
+        <div className='media-page'>
+            <h1 className='page-title'>{title || "TITULONOSE"}</h1>
+
+            <div className='section'>
+                
+            </div>
+        </div>
     );
 }
 
