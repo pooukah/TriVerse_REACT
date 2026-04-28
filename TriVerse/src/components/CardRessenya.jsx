6a55174daf9e445447b6a18aaba49ba68bdb2699
Ray <ray.palacios96@gmail.com>
2026-04-28 03:52:41 +0200

    Fet fetch amb mètode GET per mostrar reviews Refs #3

--- TriVerse/src/components/CardRessenya.jsx
+++ TriVerse/src/components/CardRessenya.jsx
@@ -1,8 +1,43 @@
+import { useState, useEffect } from 'react';
+
 function CardRessenya() {
+    const [reviews, setReviews] = useState([]);
+    
+        async function getReview() {
+            const url = "http://127.0.0.1:8000/api/reviews/?format=json";
+            try {
+                const response = await fetch(url, { method: "GET" });
+                if(response.ok) {
+                    console.log("La consulta ha anata bé");
+                } else {
+                    console.log("La consulta a tingut algun error");
+                    throw new Error(
+                        `Error: ${response.status} - ${response.statusText}`
+                    );
+                }
+                const data = await response.json();
+                console.log(data);
+                setReviews(data);
+                
+            } catch (error) {
+                console.log(error);
+            } finally {
+                console.log("Final de la consulta");
+            }
+        }
+
+        useEffect(() => {
+            getReview()
+        },  [])
+  
+        
     return (
-        <div className="div-ressenya">
-            <h2>Nom d'usuari</h2>
-            <p>Review</p>
+        <div>
+            <ul className="container-cardRessenya">
+                {reviews.map((review) => (
+                    <li className="div-ressenya">{review.user} : {review.description}</li>
+                ))}
+            </ul>
         </div>
     )
 }
