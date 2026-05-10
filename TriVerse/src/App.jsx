cdc5d286cba5b8ebbb592fcaee6d7266c1ce0f49
Ray <ray.palacios96@gmail.com>
2026-05-10 03:56:48 +0200

    Feta la pàgina per modificar la contrasenya de l'usuari Refs #1

--- TriVerse/src/App.jsx
+++ TriVerse/src/App.jsx
@@ -7,11 +7,10 @@ import { estaLogueado } from './utils';
 import MediaCard from './components/mediacard/MediaCard.jsx';
 import MediaPage from './components/MediaPage/MediaPage.jsx';
 import Home from './components/home/Home.jsx';
-import ResetPassword from './components/Profile/ResetPassword.jsx';
 import Reviews from './components/Reviews/Reviews.jsx';
 import Perfil from './components/Profile/Perfil.jsx';
 import AfegirDonacio from './components/Reviews/AfegirDonacio.jsx';
-
+import ResetPassword from './components/Profile/ResetPassword.jsx';
 function Games() {
   return <MediaPage title="Videojuegos" data={[]} />;
 }
@@ -35,10 +34,10 @@ function App(){
       </div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/resetPassword" element={<ResetPassword />} />
           <Route path="/reviews" element={<Reviews />} />
           <Route path="/afegirReview" element={<AfegirReview />} />
           <Route path="/perfil" element={<Perfil />} />
+          <Route path="/resetPassword" element={<ResetPassword />} />
           <Route path="/videojuegos" element={<Games />} />
           <Route path="/peliculas" element={<Movies />} />
           <Route path="/libros" element={<Books />} />
--- TriVerse/src/components/Profile/ResetPassword.jsx
+++ TriVerse/src/components/Profile/ResetPassword.jsx
@@ -1,6 +1,6 @@
 import eye_off_icon from '../../imatges/eye_off_icon.png';
 import eye_visible from '../../imatges/eye_visible.png';
-
+import React, { useState, useEffect } from 'react';
 
 // Funció per activar i desactivar la visibilitat de la contrasenya
 function mostrarPassword() {
@@ -22,23 +22,76 @@ function ResetPassword() {
         window.location.assign("/perfil");
     }
 
+    const id = 6;
+    
+
+    const [password, setPassword] = useState('');
+
+    const handlePassword = async () => {
+        const token = document.cookie
+            .split('; ')
+            .find(row => row.startsWith('token='))
+            ?.split('=')[1];
+
+            console.log('token:', token);
+        
+        if (!token) {
+            console.log("No hi ha token");
+            return;
+        }
+
+        const url =`http://127.0.0.1:8000/api/profilePassword/${id}/`;
+
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    password
+                }),
+            });
+
+            if (response.ok) {
+                console.log("La resposta és ok");
+                alert("Contrasenya canviada");
+            } else {
+                console.log("Error en la resposta");
+                throw new Error("Error en la resposta");
+            }
+
+            const dades = await response.json();
+            console.log("obtenim les dades", dades);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            console.log("Codi final del fetch");
+        }
+    };
+
+    
+           
     return (
         <div>
             <div className="container-reset-password">
                 <div className="div-reset-password">
                     <h1>Restablir contrasenya</h1>
                     <label>
-                        Contrasenya: <input type="password" name="contrasenya" className="input-password" /><img src={eye_off_icon} onClick={mostrarPassword} alt="password ocult" className="password-ocult"/>
+                        Contrasenya: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="contrasenya" className="input-password" /><img src={eye_off_icon} onClick={mostrarPassword} alt="password ocult" className="password-ocult"/>
+                        
                         <div className="botons-reset-password">
                             <button onClick={cancelarReset}>Cancel·lar</button>
-                            <button>Restablir</button>
+                            <button onClick={handlePassword}>Restablir</button>
                         </div>
                     </label>
                 </div>
             </div>
         </div>
-
     )
 }
+
+
 
 export default ResetPassword;
